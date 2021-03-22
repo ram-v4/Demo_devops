848f2589adbad6e5aa612f40892d61a3fe9a8548
ram-v4 <ramakrishnan.vanumamalai@lntinfotech.com>
2021-03-22 17:15:05 +0530

    error rectified

--- Demo_devops/Program.cs
+++ Demo_devops/Program.cs
@@ -10,7 +10,7 @@ namespace Demo_devops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello")
+            Console.WriteLine("Hello");
         }
     }
 }
